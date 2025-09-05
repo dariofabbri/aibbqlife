49e77ee8fcab0e350ab15d51a564d4b83b058649
Dario Fabbri <darfabbr@gmail.com>
2025-09-05 11:49:39 +0200

    Fixed init file for config flow management

--- custom_components/aibbqlife/__init__.py
+++ custom_components/aibbqlife/__init__.py
@@ -1,3 +1,24 @@
 import logging
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up integration via YAML (not used with ConfigFlow)."""
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up from a config entry (UI)."""
+    _LOGGER.debug("Setting up config entry for %s", DOMAIN)
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    _LOGGER.debug("Unloading config entry for %s", DOMAIN)
+    hass.data[DOMAIN].pop(entry.entry_id)
+    return True
